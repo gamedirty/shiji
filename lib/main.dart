7dbaedee0c459c0aad8ff135637e191fb3c6f29d
我爱吃柚子 <gamedirty@hotmail.com>
2026-09-08 00:09:23 +0800

    fix: 应用锁定竖屏，启动器名称改为「食记」
    - Android manifest 增加 screenOrientation=portrait，main.dart 设置首选竖屏
    - 系统自动旋转开关开启时应用也不再旋转（该开关本身无法被应用修改）
    - 启动器显示名 shiji → 食记

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 import 'app.dart';
@@ -6,6 +7,11 @@ import 'data/store.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  // 手机端锁定竖屏（桌面端为空实现）
+  SystemChrome.setPreferredOrientations(<DeviceOrientation>[
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(
     ChangeNotifierProvider(
       create: (_) => AppStore()..init(),
